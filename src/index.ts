03593a1d5e21f0d58253887022cbc3760f85a14d
pauljay611 <pauljay611@gmail.com>
2021-04-06 22:41:45 +0800

    add global object on window

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+import findReactInstanceByRoot from "./findReactInstanceByRoot";
+import fiberNodeParser from "./fiberNodeParser";
+
 export * from "./findReactInstanceByRoot";
 export * from "./fiberNodeParser";
+
+declare global {
+  interface Window {
+    $__react_nip: any;
+  }
+}
+
+window.$__react_nip = { findReactInstanceByRoot, fiberNodeParser };
